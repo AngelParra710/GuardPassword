0e105e4cda26adc24e91763bd40c7652b0d141ae
Angel Josue Parra Mendoza <ajpm@thinkcares.com>
2022-06-09 13:38:13 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>GuardPassword</title>
+        <meta name="description" content="Aplicacion para reguardo de password" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
